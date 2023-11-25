43a11a941ed71682d96f8e750d7eb43b016f815d
marufsiyam91 <marufsiyam9123@gmail.com>
2023-11-25 10:18:42 +0600

    [done]

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Tasbih from './Components/tasbih-counter/Tasbih-counter'
+
+
+function App() {
+
+  return (
+    <>
+        <Tasbih/>
+    </>
+  )
+}
+
+export default App
